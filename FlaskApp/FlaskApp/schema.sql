78a6bc4756eb4d742d8b8ca4f8711eb374f5dae7
root <ronit10z@gmail.com>
2016-09-17 05:14:19 +0000

    formatting changes

--- FlaskApp/FlaskApp/schema.sql
+++ FlaskApp/FlaskApp/schema.sql
@@ -4,3 +4,11 @@ create table entries (
   title text not null,
   'text' text not null
 );
+
+create table user_data (
+  id integer primary key autoincrement,
+  username text not null,
+  password text not null,
+  rating integer not null,
+  upvoted text not null
+);
